35b4eb2ef9358876883e21dfcf1a711dc3c209a1
Jacob Schoff <jschoff155@gmail.com>
2023-10-18 12:46:07 -0500

    Add project files.

---
+++ FamilyReceipeApplication/Data/Migrations/20231016184715_initialsetup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyReceipeApplication.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RecipeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecipeIngredients = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecipeInstructions = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecipeMealType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecipeCusineType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipe", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipe");
+        }
+    }
+}
---
+++ FamilyReceipeApplication/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿namespace FamilyReceipeApplication.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeIngredients { get; set; }
+        public string RecipeInstructions { get; set; }
+        public string RecipeMealType { get; set; }
+        public string RecipeCusineType { get; set; }
+
+        public Recipe()
+        {
+            
+        }
+    }
+}
